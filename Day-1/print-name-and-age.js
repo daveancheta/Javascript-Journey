87154014b761fa5fe2461debe2e0c60dff4e1aec
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-01 16:13:16 +0800

    print name and age

--- Day-1/print-name-and-age.js
+++ Day-1/print-name-and-age.js
@@ -1,5 +1,5 @@
 let myName = 'Heaven Dave Ancheta';
 let myAge = 19;
 
-console.log("Name:", myName)
-console.log("Age:", myAge)
+console.log("Name:", myName) // Output: Name: Heaven Dave Ancheta
+console.log("Age:", myAge) // Output: Age: !9
