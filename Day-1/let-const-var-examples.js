306f349ee1ee1c948b1c3f4b410c24e48b1f977f
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-01 16:33:30 +0800

    reassign let

--- Day-1/let-const-var-examples.js
+++ Day-1/let-const-var-examples.js
@@ -1,15 +1,19 @@
-// Redeclaring Var
-var myAge = 20; // right now myAge is 20
+// // Redeclaring Var
+// var myAge = 20; // right now myAge is 20
 
-var myAge = 21; // redeclared right now myAge is 21
+// var myAge = 21; // redeclared right now myAge is 21
 
-console.log(myAge); // Output: 21
+// console.log(myAge); // Output: 21
 
-// Reassign Var
-var myName = 'Heaven Dave Ancheta'; // right now my name is Heaven Dave Ancheta
+// // Reassign Var
+// var myName = 'Heaven Dave Ancheta'; // right now my name is Heaven Dave Ancheta
+// myName = 'John Doe'; // reassign my name to John Doe
+
+// console.log(myName); // Output: John Doe
+
+// Reassign Let 
+let myName = 'Dave Ancheta'; // right now my name is Dave Ancheta
 myName = 'John Doe'; // reassign my name to John Doe
 
 console.log(myName); // Output: John Doe
-
-
 
