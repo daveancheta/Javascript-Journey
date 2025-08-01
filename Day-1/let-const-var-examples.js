d4da4a5785088119e98bad78b44cb85fd8f80490
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-01 16:39:01 +0800

    let-const-var-examples

--- Day-1/let-const-var-examples.js
+++ Day-1/let-const-var-examples.js
@@ -1,4 +1,5 @@
 // // Redeclaring Var
+// let and var can be reassign, var can be redeclare while let can't. const cannot be redeclared and reassign
 // var myAge = 20; // right now myAge is 20
 
 // var myAge = 21; // redeclared right now myAge is 21
@@ -11,9 +12,14 @@
 
 // console.log(myName); // Output: John Doe
 
-// Reassign Let 
-let myName = 'Dave Ancheta'; // right now my name is Dave Ancheta
-myName = 'John Doe'; // reassign my name to John Doe
+// // Reassign Let 
+// let myName = 'Dave Ancheta'; // right now my name is Dave Ancheta
+// myName = 'John Doe'; // reassign my name to John Doe
 
-console.log(myName); // Output: John Doe
+// console.log(myName); // Output: John Doe
 
+const myName = 'Heaven Dave Ancheta';
+const myAge = 19;
+
+console.log("Name:", myName); // Output: Heaven Dave Ancheta
+console.log("Age:", myAge); // Output: 19
