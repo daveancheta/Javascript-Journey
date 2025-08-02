8a64615724e0265937d9e9bd01687203940083f3
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-02 16:56:20 +0800

    slice

--- Day-2/string-methods.js
+++ Day-2/string-methods.js
@@ -5,3 +5,8 @@ let lowercase = word.toLowerCase(); // LowerCase
 
 console.log(uppercase); // Output: HELLO WORLD
 console.log(lowercase); // Output: hello world
+
+const foods = ["Pizza", "Burger", "Chicken", "Fries", "Mango"];
+const slice = foods.slice(1, 4)
+
+console.log(slice); // Output: [ 'Burger', 'Chicken', 'Fries' ]
