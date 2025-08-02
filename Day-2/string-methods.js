224321b6717fce6c1d123886fa6bf200d4b3f4ae
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-02 16:51:25 +0800

    to lowercase

--- Day-2/string-methods.js
+++ Day-2/string-methods.js
@@ -1,5 +1,7 @@
 let word = "Hello World";
 
 let uppercase = word.toUpperCase(); // Uppercase
+let lowercase = word.toLowerCase(); // LowerCase
 
 console.log(uppercase); // Output: HELLO WORLD
+console.log(lowercase); // Output: hello world
