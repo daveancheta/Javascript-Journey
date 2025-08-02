ae39d10a8f44d34101ad2c4ea949ca31cd26ac3b
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-02 16:40:58 +0800

    string-concatenation

---
+++ Day-2/string-concatenation.js
@@ -0,0 +1,12 @@
+// Concatenate strings using + and template literals
+let firstname = "Heaven Dave";
+let lastname = "Ancheta";
+
+// Using +
+let fullname1 = firstname + " " + lastname;
+
+// Using template literals 
+let fullname2 = `${firstname} ${lastname}`;
+
+console.log("Using +:", fullname1);
+console.log("Using template literals:", fullname2);
