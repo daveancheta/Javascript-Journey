7a788dd376199fabcf257a9816e5fb2471301ce2
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-02 17:01:01 +0800

    length

--- Day-2/string-methods.js
+++ Day-2/string-methods.js
@@ -10,3 +10,8 @@ const foods = ["Pizza", "Burger", "Chicken", "Fries", "Mango"];
 const slice = foods.slice(1, 4)
 
 console.log(slice); // Output: [ 'Burger', 'Chicken', 'Fries' ]
+
+let text = "Javascript Journey";
+let length = text.length;
+
+console.log("Text Length:", length); // Output:  Text Length: 18
