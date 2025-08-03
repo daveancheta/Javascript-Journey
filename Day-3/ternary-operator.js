dd5c7167d50268b559370ace26ea2b57a99f8344
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-03 16:36:03 +0800

    Ternary operator challenge.

---
+++ Day-3/ternary-operator.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+console.log("Are you underage or of legal age?");
+let ageInput = parseInt(prompt("Enter your age: "));
+
+const output = ageInput >= 18 ? "You are of legal age." : "You are still a minor.";
+
+console.log(output);
+
