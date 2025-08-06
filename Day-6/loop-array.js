40e40df525a54efa549756231d707eded8f373b1
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-06 15:49:48 +0800

    forEach() calls a function for each element in an array

--- Day-6/loop-array.js
+++ Day-6/loop-array.js
@@ -10,3 +10,16 @@ for (let i = 2; i < length; i++) {
 }
 
 console.log(text); // Output: Cantaloupe Dragonfruit Elderberry
+
+// forEach() calls a function for each element in an array
+const fruits2 = ["Dragonfruit", "Cantaloupe", "Banana", "Apple", "Elderberry"];
+fruits2.sort();
+
+let text2 = "";
+fruits.forEach(myFunction);
+
+function myFunction(item, index) {
+    text2 += index + ". " + item + "\n";
+}
+
+console.log(text2); // Output: 0. Apple1. Banana 2. Cantaloupe 3. Dragonfruit 4. Elderberry
