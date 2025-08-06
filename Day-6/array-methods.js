c39fc675b1b0967ef1548eddab0a7ab0ed15efe2
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-06 14:53:08 +0800

    pop() removes the last element of an array

---
+++ Day-6/array-methods.js
@@ -0,0 +1,5 @@
+// pop() removes the last element of an array.
+const fruits = ["Apple", "Banana", "Cantaloupe", "Dragonfruit", "Elderberry"];
+fruits.pop();
+
+console.log(fruits); // Output: [ 'Apple', 'Banana', 'Cantaloupe', 'Dragonfruit' ]
