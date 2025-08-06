c4a95e61327bd0bcde3d4f3fb2dfbd7b6d4a81de
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-06 14:55:25 +0800

    push() adds new items to the end of an array

--- Day-6/array-methods.js
+++ Day-6/array-methods.js
@@ -3,3 +3,9 @@ const fruits = ["Apple", "Banana", "Cantaloupe", "Dragonfruit", "Elderberry"];
 fruits.pop();
 
 console.log(fruits); // Output: [ 'Apple', 'Banana', 'Cantaloupe', 'Dragonfruit' ]
+
+// push() adds new items to the end of an array
+const fruits1 = ["Apple", "Banana", "Cantaloupe", "Dragonfruit", "Elderberry"];
+fruits1.push("Fig");
+
+console.log(fruits1); // Output: [ 'Apple', 'Banana', 'Cantaloupe', 'Dragonfruit', 'Elderberry', 'Fig' ]
