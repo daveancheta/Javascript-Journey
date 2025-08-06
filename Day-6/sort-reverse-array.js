1a35d469fec00a9d643d637e8819b30262687b57
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-06 15:11:27 +0800

    The sort() method sorts an array alphabetically

---
+++ Day-6/sort-reverse-array.js
@@ -0,0 +1,4 @@
+// Sort and reverse the array
+const fruits = ["Dragonfruit", "Cantaloupe", "Banana", "Apple", "Elderberry"];
+fruits.sort();
+console.log(fruits); // Output: [ 'Apple', 'Banana', 'Cantaloupe', 'Dragonfruit', 'Elderberry' ]
