48c1505da58aa5a30aca4d7a19b81cb353af3ab3
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-06 15:14:13 +0800

    Sort and reverse the array.

--- Day-6/sort-reverse-array.js
+++ Day-6/sort-reverse-array.js
@@ -1,4 +1,9 @@
-// Sort and reverse the array
+// The sort() method sorts an array alphabetically:
 const fruits = ["Dragonfruit", "Cantaloupe", "Banana", "Apple", "Elderberry"];
 fruits.sort();
 console.log(fruits); // Output: [ 'Apple', 'Banana', 'Cantaloupe', 'Dragonfruit', 'Elderberry' ]
+
+// Sort and reverse the array.
+const fruits2 = ["Dragonfruit", "Cantaloupe", "Banana", "Apple", "Elderberry"];
+fruits2.sort().reverse();
+console.log(fruits2); // Output: [ 'Apple', 'Banana', 'Cantaloupe', 'Dragonfruit', 'Elderberry' ]
