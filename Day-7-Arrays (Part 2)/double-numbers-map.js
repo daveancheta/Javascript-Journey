f7dc67f8ffb5f9a0b00ece19a2d7a4e3916b11f2
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-07 16:45:49 +0800

    Multiply all the values in an array with 2

---
+++ Day-7-Arrays (Part 2)/double-numbers-map.js
@@ -0,0 +1,10 @@
+// Use map() to double each number.
+
+const doubleNumber = [1, 2, 3, 4, 5]
+const result = doubleNumber.map(multiplyNumber);
+
+function multiplyNumber(num) {
+    return num * 2;
+}
+
+console.log(result);
