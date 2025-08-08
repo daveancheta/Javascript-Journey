91fda1c97b2bdf568900de5fb387961b6218d277
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-08 13:39:41 +0800

    Create a person object with name, age, job

---
+++ Day-8/person-object.js
@@ -0,0 +1,8 @@
+// Create a person object with name, age, job.
+
+const person = {fullname: "Heaven Dave Ancheta", age: 19, job: "Full-Stack Developer"};
+
+console.log("Employee:")
+console.log("Fullname", person.fullname);
+console.log("Age:", person.age);
+console.log("Job:", person.job);
