9c3ca0fccbad5cef52730ce3aca01ed2719e4827
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-09 12:43:50 +0800

    dynamic-element-manipulation.js

---
+++ Day-9/dynamic-element-manipulation/dynamic-element-manipulation.js
@@ -0,0 +1,11 @@
+// Add and remove elements dynamically.
+
+// If the button is clicked, the element will hide.
+function hide() {
+    document.querySelector("h1").classList.add("hidden");
+}
+
+// If the button is clicked, the element will show.
+function show() {
+    document.querySelector("h1").classList.remove("hidden"); 
+}
