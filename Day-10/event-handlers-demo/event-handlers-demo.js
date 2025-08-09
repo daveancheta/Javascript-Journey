7d021e993781a32cdb42652f82fe13449e33eb0a
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-09 18:05:35 +0800

    event-handlers-demo (mouseover)

---
+++ Day-10/event-handlers-demo/event-handlers-demo.js
@@ -0,0 +1,10 @@
+// Mouseover
+
+function bigbutton(x) {
+    x.style.width = "200px"
+    x.style.height = "100px"
+}
+function normalButton(x) {
+    x.style.width = "100px"
+    x.style.height = "50px"
+}
