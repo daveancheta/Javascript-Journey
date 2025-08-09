bc2d1422c7158441bc3fd1086161b87475d619ec
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-09 14:00:40 +0800

    Create a counter app

---
+++ Day-9/create a counter app/counter-app.js
@@ -0,0 +1,7 @@
+// Create a counter app.
+
+let number = parseInt(document.getElementById("displayCount").innerHTML);
+
+function add() {
+    document.querySelector("h1").innerHTML +++ parseInt(number + 1);
+}
