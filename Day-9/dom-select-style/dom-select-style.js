dbf16d70050056f544e34e25e935cf7901b33e9f
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-09 12:28:52 +0800

    dom-select-style.js

---
+++ Day-9/dom-select-style/dom-select-style.js
@@ -0,0 +1,7 @@
+// Select and style elements via getElementById, querySelector.
+
+// querySelector
+document.querySelector("p").style.backgroundColor = "blue";
+
+// getElementById
+document.getElementById("example").style.backgroundColor = "red";
