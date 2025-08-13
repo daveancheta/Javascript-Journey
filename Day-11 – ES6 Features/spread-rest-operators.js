50706095fec5c4f9bd520e7b2f907cc193eef167
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-13 12:13:21 +0800

    Use spread and rest operators updated

--- Day-11 – ES6 Features/spread-rest-operators.js
+++ Day-11 – ES6 Features/spread-rest-operators.js
@@ -1,6 +1,8 @@
-const firstGroup = [1, 2, 3, 4, 5,];
-const secondGroup = [6, 7, 8, 9, 10];
+// Use spread and rest operators.
 
-const groupCombined = [...firstGroup, ...secondGroup];
+const firstGroup = [1, 2, 3, 4, 5,]; // Output: [1, 2, 3, 4, 5,]
+const secondGroup = [6, 7, 8, 9, 10]; // Output: [6, 7, 8, 9, 10]
+
+const groupCombined = [...firstGroup, ...secondGroup]; // Output: [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
 
 console.log(groupCombined);
