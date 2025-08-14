f9fddcf16a502f6e39f407597dc16c002ab08d50
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-14 14:29:13 +0800

    updated live-digital-clock

--- Day-12/live-digital-clock/live-digital-clock.js
+++ Day-12/live-digital-clock/live-digital-clock.js
@@ -6,16 +6,16 @@ const time = setInterval(timeNow, 1000)
 function timeNow() {
     const t = new Date();
 
-    let hr  = t.getHours();
-    let min = t.getMinutes();
-    let sec = t.getSeconds();
+    let hr  = t.getHours(); // Get the local hours
+    let min = t.getMinutes(); // Get the local minutes
+    let sec = t.getSeconds(); // Get the local seconds
 
-    let defaultTime = "am";
+    let defaultTime = "am"; // The default is AM if it’s before midday
 
     if (hr > 12) {
         hr -= 12;
         defaultTime = "pm";
-    }
+    } // Change the format if it is above 12 PM noon, and change the default AM to PM
 
-    document.getElementById("live-clock").innerHTML = hr + ":" + min + ":" + sec+defaultTime;
+    document.getElementById("live-clock").innerHTML = hr + ":" + min + ":" + sec+defaultTime; // display
 }
