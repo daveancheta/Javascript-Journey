ef286f92965763a9c01ae7e23d1c19cbd1fd636e
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-14 14:47:10 +0800

    updated fetch-random-joke

--- Day-12/fetch-random-joke/fetch-random-joke.js
+++ Day-12/fetch-random-joke/fetch-random-joke.js
@@ -1,7 +1,7 @@
-getJoke("fetch-joke.txt");
+getJoke("fetch-joke.txt"); // Call the function and pass the file name
 
 async function getJoke(joke) {
-    let f = await fetch(joke);
-    let display = await f.text();
-    document.getElementById("joke").innerHTML = display;
+    let f = await fetch(joke); // Fetch the file from the given path or URL
+    let display = await f.text(); // Convert the fetched response into text
+    document.getElementById("joke").innerHTML = display; // Insert the text into the HTML element with id="joke"
 }
