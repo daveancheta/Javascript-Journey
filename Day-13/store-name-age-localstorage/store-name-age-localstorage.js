69c97a14c9d9170c4a5be443c321d13fde07ebef
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-15 14:22:30 +0800

    updated store-name-age-localstorage.js with comments

--- Day-13/store-name-age-localstorage/store-name-age-localstorage.js
+++ Day-13/store-name-age-localstorage/store-name-age-localstorage.js
@@ -1,16 +1,25 @@
-// 
+// Store name and age in localStorage
 function submitButton() {
+
+    // Get the value from the input field with id "fullname"
     const fullname = document.getElementById("fullname").value;
+
+    // Get the value from the input field with id "age"
     const age = document.getElementById("age").value;
 
-
+    // Store the fullname value in localStorage
     localStorage.setItem("fullname", fullname);
+
+    // Store the age value in localStorage
     localStorage.setItem("age", age);
 
+    // Retrieve the fullname value from localStorage
     let displayName = localStorage.getItem("fullname");
+
+    // Retrieve the fullname value from localStorage
     let displayAge = localStorage.getItem("age");
 
-
+    // Display the fullname and age values in HTML elements
     document.getElementById("displaySaveName").innerHTML = displayName;
     document.getElementById("displaySaveAge").innerHTML = displayAge;
 }
