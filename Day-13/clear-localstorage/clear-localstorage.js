a243d77369da2c101d36c0fe2736608d50d138d2
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-15 16:28:10 +0800

    add to cart function

---
+++ Day-13/clear-localstorage/clear-localstorage.js
@@ -0,0 +1,23 @@
+function submitForm(event, form) {
+    event.preventDefault();
+    
+    let product = form.elements["product"].value;
+    let price = form.elements["price"].value;
+
+    const newProduct = {product, price};
+
+    let saveProduct = JSON.parse(localStorage.getItem("product")) || [];
+    saveProduct.push(newProduct);
+    localStorage.setItem("product", JSON.stringify(saveProduct));
+
+
+    location.reload();
+}
+
+  let products = JSON.parse(localStorage.getItem("product"));
+  let cart = "";
+    products.forEach(p => {
+        cart += `<li>${p.product} - $${p.price}</li><br>`
+    });
+
+    document.getElementById("displayCart").innerHTML = cart;
